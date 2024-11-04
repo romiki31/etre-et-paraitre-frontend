0ab35ab2194912cfdc7f70d50ad25fd7b8471f16
Donatello SCARABINO <donatelloscarabino@outlook.fr>
2024-11-04 14:20:06 +0100

    ddd

--- server/server.ts
+++ server/server.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5001;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://donska.fr"],
+    origin: ["http://localhost:5173", "https://donska.fr", "http://donska.fr"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   },
